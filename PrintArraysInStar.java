03bd4ee13f5a3a262c5a00c4a27b90acb18e8e14
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 21:24:04 +0530

    Create PrintArraysInStar.java

---
+++ PrintArraysInStar.java
@@ -0,0 +1,26 @@
+package Practicecodes.Java.Partiallyopenbook;
+
+import java.util.Scanner;
+
+public class PrintArraysInStar {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of items");
+        Scanner scan=new Scanner(System.in);
+        final int MAX_ITEM=scan.nextInt();
+        int[] arr=new int[MAX_ITEM];
+        System.out.println("Enter the Items of the array");
+        if(MAX_ITEM>0)
+        for(int i=0;i<MAX_ITEM;i++){
+            arr[i]=scan.nextInt();
+        }
+        scan.close();
+        System.out.println("\nThe elements of the arrays are ");
+        for(int i=0;i<MAX_ITEM;i++){
+            for(int star=1;star<=arr[i];star++){
+                System.out.print("*");
+            }
+            System.out.print("("+arr[i]+")");
+            System.out.println();
+        }
+    }
+}
