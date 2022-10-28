0ee98ce2f78e46cb6260a54f90af5b2c1c99eb44
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:57:31 +0530

    Create CheckOddEven.java

---
+++ CheckOddEven.java
@@ -0,0 +1,14 @@
+package Practicecodes.Java.Partiallyopenbook;
+
+public class CheckOddEven {
+    public static void main(String[] args) { // Program entry point
+        int number = 49; // Set the value of "number" here!
+        System.out.println("The number is " + number); 
+        if (number%2==0) { 
+        System.out.println("Even number"); // even number
+        } else { 
+        System.out.println("Odd number"); // odd number
+        } 
+        System.out.println("Bye"); 
+        } 
+}
